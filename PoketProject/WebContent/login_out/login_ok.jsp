491347091c75dcc68c6f2163e514a868fe802505
hyunna <hyunna7044@naver.com>
2017-04-19 14:06:33 +0900

    memberdao 수정 login_ok 수정

--- PoketProject/WebContent/login_out/login_ok.jsp
+++ PoketProject/WebContent/login_out/login_ok.jsp
@@ -6,46 +6,46 @@
 <%@page import="javafx.scene.control.Alert" %>
 <jsp:useBean id="accessUserCount" class="java.util.ArrayList" scope="application" />
 <%
-	String id=request.getParameter("id");
-	String pwd=request.getParameter("pwd");
-	
-	//DB연동
-	MemberDAO dao=MemberDAO.newInstance();
-	int res=dao.isLogin(id, pwd);
-	
-	if(res==0){
+   String id=request.getParameter("id");
+   String pwd=request.getParameter("pwd");
+   
+   //DB연동
+   MemberDAO dao=MemberDAO.newInstance();
+   int res=dao.isLogin(id, pwd);
+   
+   if(res==0){
 %>
-	<script>
-		alert("ID를 확인하세요.")
-		history.back();
-	</script>
+   <script>
+      alert("ID를 확인하세요.")
+      history.back();
+   </script>
 <%
-	}else if(res==1){
+   }else if(res==1){
 %>
-	<script>
-		alert("비밀번호가 틀렸습니다.")
-		history.back();
-	</script>
+   <script>
+      alert("비밀번호가 틀렸습니다.")
+      history.back();
+   </script>
 <%
-	}else if(res==2){
-		MemberVO vo = new MemberVO();
-		
-		MemberVO info=dao.getLogData(id);
-		session.setAttribute("id", id);
-		session.setAttribute("name", info.getNickname());
-		session.setAttribute("email", info.getEmail());
-		session.setAttribute("point", info.getPoint());
-		
-		//접속자 리스트에 추가
-		HashMap userInfo = new HashMap();
-		userInfo.put("sessionId",session.getId());
-		userInfo.put("id",id);
-		userInfo.put("name",info.getNickname());
-		userInfo.put("accessTime",System.currentTimeMillis());
-		accessUserCount.add(userInfo);
-		System.out.println(id);
-		
-		//초기 페이지
-		response.sendRedirect("../main/main.jsp");
-	}
+   }else if(res==2){
+      MemberVO vo = new MemberVO();
+      
+      MemberVO info=dao.getLogData(id);
+      session.setAttribute("id", id);
+      session.setAttribute("name", info.getNickname());
+      session.setAttribute("email", info.getEmail());
+      session.setAttribute("point", info.getPoint());
+      
+      //접속자 리스트에 추가
+      HashMap userInfo = new HashMap();
+      userInfo.put("sessionId",session.getId());
+      userInfo.put("id",id);
+      userInfo.put("name",info.getNickname());
+      userInfo.put("accessTime",System.currentTimeMillis());
+      accessUserCount.add(userInfo);
+      System.out.println(id);
+      
+      //초기 페이지
+      response.sendRedirect("../main/main.jsp");
+   }
 %>
--- PoketProject/src/com/sist/user/MemberDAO.java
+++ PoketProject/src/com/sist/user/MemberDAO.java
@@ -7,271 +7,309 @@ import com.sun.org.apache.regexp.internal.RE;
 import java.sql.*;
 
 public class MemberDAO {
-	private Connection conn;
-	private PreparedStatement ps; // @211.238.142.235
-	private final String URL = "jdbc:oracle:thin:@211.238.142.235:1521:ORCL";
-	private static MemberDAO dao;
+   private Connection conn;
+   private PreparedStatement ps; // @211.238.142.235
+   private final String URL = "jdbc:oracle:thin:@211.238.142.235:1521:ORCL";
+   private static MemberDAO dao;
 
-	public MemberDAO() {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch (Exception ex) {
-			System.out.println("MemberDAO()" + ex.getMessage());
-		}
-	}
+   public MemberDAO() {
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+      } catch (Exception ex) {
+         System.out.println("MemberDAO()" + ex.getMessage());
+      }
+   }
 
-	public static MemberDAO newInstance() {
-		if (dao == null) {
-			dao = new MemberDAO();
-		}
-		return dao;
-	}
+   public static MemberDAO newInstance() {
+      if (dao == null) {
+         dao = new MemberDAO();
+      }
+      return dao;
+   }
 
-	public void getConnection() {
-		try {
-			conn = DriverManager.getConnection(URL, "scott", "tiger");
-		} catch (Exception ex) {
-			System.out.println("getConnection()" + ex.getMessage());
-		}
-	}
-	
-	public void disConnection() {
-		try {
-			if (ps != null)
-				ps.close();
-			if (conn != null)
-				conn.close();
-		} catch (Exception ex) {
-			System.out.println("disConnection()" + ex.getMessage());
-		}
-	}
+   public void getConnection() {
+      try {
+         conn = DriverManager.getConnection(URL, "scott", "tiger");
+      } catch (Exception ex) {
+         System.out.println("getConnection()" + ex.getMessage());
+      }
+   }
+   
+   public void disConnection() {
+      try {
+         if (ps != null)
+            ps.close();
+         if (conn != null)
+            conn.close();
+      } catch (Exception ex) {
+         System.out.println("disConnection()" + ex.getMessage());
+      }
+   }
 
-	public int isLogin(String id, String pwd) {
-		int result = 0;
+   public int isLogin(String id, String pwd) {
+      int result = 0;
 
-		try {
-			getConnection();
-			String sql = "SELECT COUNT(*) FROM join_user " + "WHERE id=?";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, id);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			int count = rs.getInt(1);
+      try {
+         getConnection();
+         String sql = "SELECT COUNT(*) FROM join_user " + "WHERE id=?";
+         ps = conn.prepareStatement(sql);
+         ps.setString(1, id);
+         ResultSet rs = ps.executeQuery();
+         rs.next();
+         int count = rs.getInt(1);
 
-			if (count == 0) {
-				result = 0;
-			} else {
-				sql = "SELECT pwd FROM join_user WHERE id=?";
-				ps = conn.prepareStatement(sql);
-				ps.setString(1, id);
-				rs = ps.executeQuery();
-				rs.next();
-				String db_pwd = rs.getString(1);
-				rs.close();
+         if (count == 0) {
+            result = 0;
+         } else {
+            sql = "SELECT pwd FROM join_user WHERE id=?";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            rs.next();
+            String db_pwd = rs.getString(1);
+            rs.close();
 
-				if (db_pwd.equals(pwd)) {
-					result = 2;
-				} else {
-					result = 1;
-				}
-			}
-			rs.close();
+            if (db_pwd.equals(pwd)) {
+               result = 2;
+            } else {
+               result = 1;
+            }
+         }
+         rs.close();
 
-		} catch (Exception ex) {
-			System.out.println("isLogin()" + ex.getMessage());
-		} finally {
-			disConnection();
-		}
+      } catch (Exception ex) {
+         System.out.println("isLogin()" + ex.getMessage());
+      } finally {
+         disConnection();
+      }
 
-		return result;
-	}
+      return result;
+   }
 
-	public String getLogData(String id) {
-		String result = "";
-		try {
-			getConnection();
-			String sql = "SELECT nickname FROM join_user WHERE id=?";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, id);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			result = rs.getString(1);
-			rs.close();
+   public MemberVO getLogData(String id) {
+      MemberVO result=new MemberVO();
+      try {
+         getConnection();
+         String sql = "SELECT nickname,email,point FROM join_user WHERE id=?";
+         ps = conn.prepareStatement(sql);
+         ps.setString(1, id);
+         ResultSet rs = ps.executeQuery();
+         rs.next();
+         result.setNickname(rs.getString(1));
+         result.setEmail(rs.getString(2));
+         result.setPoint(rs.getInt(3));
+         
+         rs.close();
 
-		} catch (Exception ex) {
-			System.out.println("getLogData()" + ex.getMessage());
-		} finally {
-			disConnection();
-		}
-		return result;
-	}
+      } catch (Exception ex) {
+         System.out.println("getLogData()" + ex.getMessage());
+      } finally {
+         disConnection();
+      }
+      return result;
+   }
 
-	public int idcheck(String id) {
-		int count = 0;
+   public int idcheck(String id) {
+      int count = 0;
 
-		try {
-			getConnection();
-			String sql = "SELECT COUNT(*) FROM join_user " + "WHERE id=?";
+      try {
+         getConnection();
+         String sql = "SELECT COUNT(*) FROM join_user " + "WHERE id=?";
 
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, id);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			count = rs.getInt(1);
-			rs.close();
+         ps = conn.prepareStatement(sql);
+         ps.setString(1, id);
+         ResultSet rs = ps.executeQuery();
+         rs.next();
+         count = rs.getInt(1);
+         rs.close();
 
-		} catch (Exception ex) {
-			System.out.println("idcheck" + ex.getMessage());
-		} finally {
-			disConnection();
-		}
-		return count;
+      } catch (Exception ex) {
+         System.out.println("idcheck" + ex.getMessage());
+      } finally {
+         disConnection();
+      }
+      return count;
 
-	}
+   }
 
-	public void MemberInsert(MemberVO vo) {
-		try {
-			getConnection();
-			String sql = "INSERT INTO join_user VALUES(?,?,?,?,?,?,?,?,?)";
+   public void MemberInsert(MemberVO vo) {
+      try {
+         getConnection();
+         String sql = "INSERT INTO join_user VALUES(?,?,?,?,?,?,?,?,?)";
+         System.out.println(sql);
 
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, vo.getId());
-			ps.setString(2, vo.getPwd());
-			ps.setString(3, vo.getNickname());
-			ps.setString(4, vo.getGender());
-			ps.setString(5, vo.getPhone());
-			ps.setString(6, vo.getPhone1());
-			ps.setString(7, vo.getEmail());
-			ps.setInt(8, vo.getExp());
-			ps.setInt(9, vo.getPoint());
-			ps.executeUpdate();
+         ps = conn.prepareStatement(sql);
+         ps.setString(1, vo.getId());
+         ps.setString(2, vo.getPwd());
+         ps.setString(3, vo.getNickname());
+         ps.setString(4, vo.getGender());
+         ps.setString(5, vo.getPhone());
+         ps.setString(6, vo.getPhone1());
+         ps.setString(7, vo.getEmail());
+         ps.setInt(8, vo.getExp());
+         ps.setInt(9, vo.getPoint());
+         ps.executeUpdate();
 
-		} catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		} finally {
-			disConnection();
-		}
+      } catch (Exception ex) {
+         System.out.println(ex.getMessage());
+      } finally {
+         disConnection();
+      }
 
-		/*
-		 * public void updateMember(MemberVO member) throws
-		 * SQLException{
-		 * 
-		 * Connection conn=null; PreparedStatement pstmt=null;
-		 * 
-		 * try{ StringBuffer query=new StringBuffer();
-		 * 
-		 * query.append("UPDATE JSP_MEMBER SET");
-		 * query.append(" PASSWORD=?, MAIL=?, PHONE=?, ADDRESS=?");
-		 * query.append(" WHERE ID=?");
-		 * 
-		 * conn=DBConnection.getConnee
-		 * 
-		 * }catch(Exception ex){
-		 * 
-		 * } }
-		 */
+      /*
+       * public void updateMember(MemberVO member) throws
+       * SQLException{
+       * 
+       * Connection conn=null; PreparedStatement pstmt=null;
+       * 
+       * try{ StringBuffer query=new StringBuffer();
+       * 
+       * query.append("UPDATE JSP_MEMBER SET");
+       * query.append(" PASSWORD=?, MAIL=?, PHONE=?, ADDRESS=?");
+       * query.append(" WHERE ID=?");
+       * 
+       * conn=DBConnection.getConnee
+       * 
+       * }catch(Exception ex){
+       * 
+       * } }
+       */
 
-	}
-	
-	public MemberVO getMember(String id) {
-		MemberVO vo = new MemberVO();
-		try {
-			getConnection();
-			String sql = "SELECT * FROM join_user WHERE id = ?";
+   }
+   
+   public MemberVO getMember(String id) {
+      MemberVO vo = new MemberVO();
+      try {
+         getConnection();
+         String sql = "SELECT * FROM join_user WHERE id = ?";
 
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, id);
-			ResultSet rs = ps.executeQuery();
-			
-			rs.next();
-			
-			vo.setId(rs.getString(1));
-			vo.setPwd(rs.getString(2));
-			vo.setNickname(rs.getString(3));
-			vo.setGender(rs.getString(4));
-			vo.setPhone(rs.getString(5));
-			vo.setPhone1(rs.getString(6));
-			vo.setEmail(rs.getString(7));
-			vo.setExp(rs.getInt(8));
-			vo.setPoint(rs.getInt(9));
-			rs.close();
-			ps.close();
-			
+         ps = conn.prepareStatement(sql);
+         ps.setString(1, id);
+         ResultSet rs = ps.executeQuery();
+         
+         rs.next();
+         
+         vo.setId(rs.getString(1));
+         vo.setPwd(rs.getString(2));
+         vo.setNickname(rs.getString(3));
+         vo.setGender(rs.getString(4));
+         vo.setPhone(rs.getString(5));
+         vo.setPhone1(rs.getString(6));
+         vo.setEmail(rs.getString(7));
+         vo.setExp(rs.getInt(8));
+         vo.setPoint(rs.getInt(9));
+         rs.close();
+         ps.close();
+         
 
-		} catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		} finally {
-			disConnection();
-		}
-		
-		return vo;
-	}
-	
-	public void updateMember(MemberVO vo) {
-		getConnection();
-		try{
-//		String sql = "UPDATE JOIN_USER "
-//				+ "SET PWD="+vo.getPwd()+","
-//				+ "NICKNAME="+vo.getNickname()+","
-//				+ "GENDER="+vo.getGender()+","
-//				+ "PHONE="+vo.getPhone()+","
-//				+ "PHONE1="+vo.getPhone1()+","
-//				+ "EMAIL="+vo.getEmail()+","
-//				+ "EMAIL1="+vo.getEmail1()+","
-//				+ "WHERE id="+vo.getId();
-//		ps=conn.prepareStatement(sql);
-		
-		String sql = "UPDATE JOIN_USER "
-			+ "SET PWD=?,NICKNAME=?,GENDER=?,PHONE=?,PHONE1=?,EMAIL=? "
-			+ "WHERE id=?" ;
-		
-		System.out.println(sql);
-		ps=conn.prepareStatement(sql);
-	
-		ps.setString(1, vo.getPwd());
-		ps.setString(2, vo.getNickname());		
-		ps.setString(3, vo.getGender());
-		ps.setString(4, vo.getPhone());
-		ps.setString(5, vo.getPhone1());
-		ps.setString(6, vo.getEmail());
-		ps.setString(7, vo.getId());
-		
-		ps.executeUpdate();
-		ps.close();
-		
-		
-		} catch (Exception ex) {
-			ex.printStackTrace();
-//			System.out.println(ex.getMessage());
-		} finally {
-			disConnection();
-		}
-		
-	}
-	
-	public void levUp(MemberVO vo, int point, int exp) {
-		
-		try {
-			getConnection();
-			System.out.println("test1");
-			String sql = "UPDATE JOIN_USER SET point=?, exp=?"
-					+ "WHERE id=?";
-			System.out.println("test2");
-			ps=conn.prepareStatement(sql);
-			
-			ps.setInt(1, vo.getPoint()+point);
-			ps.setInt(2, vo.getExp()+exp);
-			ps.setString(3, vo.getId());		
+      } catch (Exception ex) {
+         System.out.println(ex.getMessage());
+      } finally {
+         disConnection();
+      }
+      
+      return vo;
+   }
+   
+   public void updateMember(MemberVO vo) {
+      getConnection();
+      try{
+//      String sql = "UPDATE JOIN_USER "
+//            + "SET PWD="+vo.getPwd()+","
+//            + "NICKNAME="+vo.getNickname()+","
+//            + "GENDER="+vo.getGender()+","
+//            + "PHONE="+vo.getPhone()+","
+//            + "PHONE1="+vo.getPhone1()+","
+//            + "EMAIL="+vo.getEmail()+","
+//            + "EMAIL1="+vo.getEmail1()+","
+//            + "WHERE id="+vo.getId();
+//      ps=conn.prepareStatement(sql);
+      
+      String sql = "UPDATE JOIN_USER "
+         + "SET PWD=?,NICKNAME=?,GENDER=?,PHONE=?,PHONE1=?,EMAIL=? "
+         + "WHERE id=?" ;
+      
+      System.out.println(sql);
+      ps=conn.prepareStatement(sql);
+   
+      ps.setString(1, vo.getPwd());
+      ps.setString(2, vo.getNickname());      
+      ps.setString(3, vo.getGender());
+      ps.setString(4, vo.getPhone());
+      ps.setString(5, vo.getPhone1());
+      ps.setString(6, vo.getEmail());
+      ps.setString(8, vo.getId());
+      
+      ps.executeUpdate();
+      ps.close();
+      
+      
+      } catch (Exception ex) {
+         ex.printStackTrace();
+//         System.out.println(ex.getMessage());
+      } finally {
+         disConnection();
+      }
+      
+   }
+   
+   public void levUp(MemberVO vo, int point, int exp) {
+      
+      try {
+         getConnection();
+         System.out.println("test1");
+         String sql = "UPDATE JOIN_USER SET point=?, exp=?"
+               + "WHERE id=?";
+         System.out.println("test2");
+         ps=conn.prepareStatement(sql);
+         
+         ps.setInt(1, vo.getPoint()+point);
+         ps.setInt(2, vo.getExp()+exp);
+         ps.setString(3, vo.getId());      
 
-			ps.executeUpdate();
-			ps.close();
+         ps.executeUpdate();
+         ps.close();
 
-		} catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		} finally {
-			disConnection();
-		}
+      } catch (Exception ex) {
+         System.out.println(ex.getMessage());
+      } finally {
+         disConnection();
+      }
 
-	}
+   }
+   
+   //포인트 차감
+   public MemberVO minusPoint(int point, String id){   
+      MemberVO vo = new MemberVO();
+      try{
+         getConnection();
+         String sql = "UPDATE JOIN_USER SET point=point-? WHERE id=?";
+         
+         ps=conn.prepareStatement(sql);
+         ps.setInt(1, point);
+         ps.setString(2, id);
+         ps.executeUpdate();
+         ps.close();
+         
+         sql = "SELECT id,email,point FROM JOIN_USER WHERE id=?";
+         ps=conn.prepareStatement(sql);
+         ps.setString(1, id);
+         ResultSet rs = ps.executeQuery();
+         rs.next();
+         vo.setId(rs.getString(1));
+         vo.setEmail(rs.getString(2));
+         vo.setPoint(rs.getInt(3));
+         rs.close();
+         ps.close();
+         
+         System.out.println("minusPoint");
+         
+      }catch(Exception ex){
+         System.out.println("minusPoint()"+ex.getMessage());
+      }finally{
+         disConnection();
+      }
+      return vo;
+   }
 
 }
